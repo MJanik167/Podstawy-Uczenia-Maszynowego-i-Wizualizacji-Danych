f3b1e90d61ff55e9d78f0bedc9a1848d925c30a3
MJanik167 <mjanik167@gmail.com>
2024-04-08 16:18:33 +0200

    njnjinkjnjk

--- wykres_NYT.py
+++ wykres_NYT.py
@@ -1,4 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import random
 import matplotlib as mpl
+
+def sillyCurve(x):
+    return(pow(1/4*x-8,2)/600) - 0.05
+
+X = []
+Y = range(1880,2018)
+
+def generateData():
+    for i in range(1,139):
+        print()
+        X.append(random.randrange(-200,200)/1000+sillyCurve(i))
+
+generateData()
+
+ax = plt.subplot()
+ax.plot(Y,X,color="gray",marker=".")
+ax.spines[["top","right","left","bottom"]].set_visible(False)
+ax.tick_params(colors='gray',which="both")
+plt.show()
